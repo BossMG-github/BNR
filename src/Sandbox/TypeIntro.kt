25bb52c2967f284b5f0f15c600342ef381964098
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-01 11:59:28 +0900

    [2024-10-31] Sandbox TypeIntro

--- src/Sandbox/TypeIntro.kt
+++ src/Sandbox/TypeIntro.kt
@@ -1,7 +1,11 @@
 package Sandbox
 
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main(args: Array<String>) {
-    var experiencePoints: Int = 5
+    val playerName = "에스트라곤"
+    var experiencePoints= 5
     experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
 }
