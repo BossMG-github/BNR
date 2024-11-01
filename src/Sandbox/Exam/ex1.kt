cc8d5b33edbaac4d08d9071a15b601704956568b
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-01 17:37:34 +0900

    [2024-10-31] Sandbox TypeIntro

---
+++ src/Sandbox/Exam/ex1.kt
@@ -0,0 +1,14 @@
+package Sandbox.Exam
+
+fun main(args: Array<String>) {
+    var hasSteed = "아직 획득하지 못했습니다."
+    var bar = "Unicorn's Hom"
+    var barMaster = "Hom"
+    var playerGold = 50
+
+    var barMenu1 = "벌꿀주"
+    var barMenu2 = "포도주"
+    var barMenu3 = "라크루아"
+    var playerName = "에스트라곤"
+    println(playerName.reversed())
+}
