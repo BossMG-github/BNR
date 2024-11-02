972db863ca394a16a8728b10dafbb9a640679e34
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-02 20:22:38 +0900

    [2024-11-02] NyetHack Game

--- src/NyetHack/Game.kt
+++ src/NyetHack/Game.kt
@@ -2,7 +2,7 @@ package NyetHack
 
 fun main(args: Array<String>){
     val name = "마드리갈"
-    var healthPoints = 100
+    var healthPoints = 89
     if(healthPoints == 100){
         println(name + " 최상의 상태임!")
     } else {
