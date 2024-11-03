5cc1a603acb4f7b6601c949155b5ff797e4e7c14
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-04 00:41:00 +0900

    [2024-11-02] NyetHack Game

--- src/NyetHack/Game.kt
+++ src/NyetHack/Game.kt
@@ -9,7 +9,6 @@ fun main(args: Array<String>) {
     //아우라
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
 
     val healthStatus = when (healthPoints) {
         100 -> " 최상의 상태임!"
@@ -19,10 +18,21 @@ fun main(args: Array<String>) {
         } else {
             " 경미한 상처만 있음."
         }
+
         in 15..74 -> " 많이 다친 것 같음."
         else -> " 최악의 상태임!"
     }
     // 플레이어의 상태 출력
-    println(name + " " + healthStatus)
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${
+                    if (isBlessed) {
+                        "YES"
+                    } else {
+                        "NO"
+                    }
+                })"
+    )
+    println("$name $healthStatus")
 
 }
