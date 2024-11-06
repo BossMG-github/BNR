b1ee9e8602778589c98de2936d42e59923b3406c
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-06 20:37:19 +0900

    [2024-11-06] NyetHack Game

--- src/NyetHack/Game.kt
+++ src/NyetHack/Game.kt
@@ -12,7 +12,7 @@ fun main(args: Array<String>) {
 
     // 플레이어의 상태 출력
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-    castFireball(5)
+    castFireball()
 
 }
 
@@ -50,6 +50,6 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     return healthStatus
 }
 
-private fun castFireball(numFireballs: Int) {
+private fun castFireball(numFireballs: Int = 2) {
     println("한 덩어리의 파이어볼이 나타난다. (x$numFireballs)")
 }
