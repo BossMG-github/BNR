b3ea94a6c78369a73b2adade93f33b9ca5439ab5
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-07 18:09:15 +0900

    [2024-11-07] Sandbox

--- src/Sandbox/SimVillage.kt
+++ src/Sandbox/SimVillage.kt
@@ -1,9 +1,9 @@
 package Sandbox
 
 fun main(args: Array<String>) {
-    println({
+    val greetingFunction: () -> String = {
         val currentYear = 2019
-        "SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear"
-    }())
-
+        "SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear)"
+    }
+    println(greetingFunction())
 }
