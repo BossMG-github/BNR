5e8a642b1605bc7a671abd9e2b35627edcfb591b
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-08 16:33:22 +0900

    [2024-11-07] Sandbox

--- src/Sandbox/SimVillage.kt
+++ src/Sandbox/SimVillage.kt
@@ -1,10 +1,11 @@
 package Sandbox
 
 fun main(args: Array<String>) {
-    val greetingFunction: (String) -> String = {
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2019
-        "SimVillage 방문을 환영합니다, $it 님! (copyright $currentYear)"
+        println("$numBuildings 채의 건물이 추가됨")
+        "SimVillage 방문을 환영합니다, $playerName! (copyright $currentYear)"
     }
 
-    println(greetingFunction("김선달"))
+    println(greetingFunction("김선달",2))
 }
