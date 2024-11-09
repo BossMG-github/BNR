09de3ce61cef3487ff8ed1dd100b3be890b8c408
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-09 23:04:31 +0900

    [2024-11-07] NyetHack

---
+++ src/NyetHack/SwordJuggler.kt
@@ -0,0 +1,11 @@
+package NyetHack
+
+fun main() {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if(isJugglingProficient){
+        swordsJuggling = 2
+    }
+    swordsJuggling = swordsJuggling!!.plus(1)
+    println("$swordsJuggling 개의 칼로 저글링합니다!")
+}
