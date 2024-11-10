18f334861acc5bdf61de2f3545d66ccaf48c0f62
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-10 22:49:41 +0900

    [2024-11-10] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -1,14 +1,12 @@
 package NyetHack
 
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
-//    var test1: String? = null
-    var beverage = readLine()
-//    beverage = null
-    if (beverage != null){
-        beverage = beverage.capitalize()
-    } else {
-        println("beverage가 null입니다!")
-    }
-    val beverageServed: String = beverage ?: "맥주"
-    println(beverageServed)
+
+}
+
+private fun placeOrder(){
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리같은 $tavernMaster 에게 주문한다.")
 }
