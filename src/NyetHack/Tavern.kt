ee5ad0a19e6b1674f3b900384a63a836a53be06c
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-11 19:18:37 +0900

    [2024-11-10] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -2,7 +2,13 @@ package NyetHack
 
 const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
-    placeOrder("shandy,Dragon's Breath, 5.91")
+    // placeOrder("shandy,Dragon's Breath, 5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+//    "Dragon's Breath".forEach {
+//        print("$it\n")
+//    }
+    //대문자도 번역 가능
+    println(toDragonSpeak("DRAGON'S BREATH: IT'S GOT WHAT ADVENTURERS CRAVE!"))
 }
 
 private fun placeOrder(menuData: String){
@@ -13,4 +19,24 @@ private fun placeOrder(menuData: String){
     val (type, name, price) = menuData.split(',')
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
+
+    val phrase = if(name == "Dragon's Breath") {
+        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다 $name"
+    }
+
+    println(phrase)
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value.lowercase()){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
