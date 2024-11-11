7d5d9092dd21cfa7cb72154d85d537849ab4e115
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-11 16:23:48 +0900

    [2024-11-10] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -2,11 +2,15 @@ package NyetHack
 
 const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
-
+    placeOrder("shandy,Dragon's Breath, 5.91")
 }
 
-private fun placeOrder(){
+private fun placeOrder(menuData: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("마드리같은 $tavernMaster 에게 주문한다.")
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
 }
