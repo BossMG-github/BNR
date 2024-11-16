6c2394cb231c369110f9c3fb606be42b71729f84
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-16 23:16:53 +0900

    [2024-11-14] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -8,9 +8,12 @@ const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Farnsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
 val menuList = File("C:\\study\\kotlin\\BNR\\src\\NyetHack\\data\\tavern-menu-items.txt").readText().split("\r\n")
 
 fun main(args: Array<String>) {
+
     if (patronList.contains("Eli")) {
         println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
     } else {
@@ -23,13 +26,18 @@ fun main(args: Array<String>) {
         println("술집 주인이 말한다: 아니오, 나간 사람도 있습니다.")
     }
 
-    patronList.forEachIndexed { index, patron ->
-        println("좋은 밤입니다, $patron 님 - 당신은 #${index + 1} 번째입니다.")
-        placeOrder(patron, menuList.shuffled().first())
+    (0..9).forEach { // 범위는 Iterable타입이라서 forEach 사용 가능
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
     }
+    println(uniquePatrons)
 
-    menuList.forEachIndexed { index, data ->
-        println("$index : $data")
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
     }
 }
 
