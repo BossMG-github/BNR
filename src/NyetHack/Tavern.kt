17e4cde1e1fdc83139f721ec7a1b19c8055dfd3e
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-16 13:53:17 +0900

    [2024-11-14] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -8,9 +8,20 @@ var playerSilver = 10
 val patronList = listOf("Eli","Mordoc","Sophie")
 
 fun main(args: Array<String>) {
+    if(patronList.contains("Eli")) {
+        println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
+    } else {
+        println("술집 주인이 말한다: Eli는 여기 없어요.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie","Mordoc"))){
+        println("술집 주인이 말한다: 네, 모두 있어요.")
+    } else {
+        println("술집 주인이 말한다: 아니오, 나간 사람도 있습니다.")
+    }
+
     placeOrder("shandy,Dragon's Breath,5.91")
 
-    println(patronList[0])
 }
 
 fun performPurchase(price: Double){
