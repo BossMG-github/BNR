dc07f8cd38774106ef6d1e13614880a9117b3392
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-16 19:06:13 +0900

    [2024-11-14] NyetHack

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -1,41 +1,43 @@
 package NyetHack
+
 import kotlin.math.roundToInt
+import java.io.File
 
 const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val menuList = File("C:\\study\\kotlin\\BNR\\src\\NyetHack\\data\\tavern-menu-items.txt").readText().split("\r\n")
 
 fun main(args: Array<String>) {
-    if(patronList.contains("Eli")) {
+    if (patronList.contains("Eli")) {
         println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
     } else {
         println("술집 주인이 말한다: Eli는 여기 없어요.")
     }
 
-    if (patronList.containsAll(listOf("Sophie","Mordoc"))){
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
         println("술집 주인이 말한다: 네, 모두 있어요.")
     } else {
         println("술집 주인이 말한다: 아니오, 나간 사람도 있습니다.")
     }
 
-    placeOrder("shandy,Dragon's Breath,5.91")
+    patronList.forEachIndexed { index, patron ->
+        println("좋은 밤입니다, $patron 님 - 당신은 #${index + 1} 번째입니다.")
+        placeOrder(patron, menuList.shuffled().first())
+    }
 
-    println(patronList)
-    patronList.remove("Eli")
-    patronList.add("Alex")
-    patronList.add(0, "Alex")
-    patronList[0] = "Alexis"
-    println(patronList)
-
+    menuList.forEachIndexed { index, data ->
+        println("$index : $data")
+    }
 }
 
-fun performPurchase(price: Double){
+fun performPurchase(price: Double) {
     displayBalance()
     val totalPurse = playerGold + (playerSilver / 100.0)
     println("지갑 전체 금액: 금화 $totalPurse")
-    if(price <= totalPurse){
+    if (price <= totalPurse) {
         println("금화 $price 로 술을 구압함")
         val remainingBalance = totalPurse - price
         println("남은 잔액: ${"%.2f".format(remainingBalance)}")
@@ -46,38 +48,37 @@ fun performPurchase(price: Double){
         playerSilver = remainingSilver
         displayBalance()
     } else {
-        println("[바텐더]: 금화: ${(price-totalPurse).roundToInt()}가 부족합니다.")
+        println("[바텐더]: 금화: ${(price - totalPurse).roundToInt()}가 부족합니다.")
     }
 
 }
 
-private fun displayBalance(){
+private fun displayBalance() {
     println("플레이어의 지갑 잔액: 금화: $playerGold 개, 은화: $playerSilver 개")
 }
 
-private fun placeOrder(menuData: String){
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("마드리갈은 $tavernMaster 에게 주문한다.")
+    println("$patronName 은 $tavernMaster 에게 주문한다")
 
     val (type, name, price) = menuData.split(',')
-    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    val message = "$patronName 은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
 
-    performPurchase(price.toDouble())
+//    performPurchase(price.toDouble())
 
-    val phrase = if(name == "Dragon's Breath") {
-        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName 이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
     } else {
-        "마드리갈이 말한다: 감사합니다 $name"
+        "$patronName 이 말한다: 감사합니다 $name"
     }
-
     println(phrase)
 }
 
 private fun toDragonSpeak(phrase: String) =
     phrase.replace(Regex("[aeiouAEIOU]")) {
-        when (it.value.lowercase()){
+        when (it.value.lowercase()) {
             "a" -> "4"
             "e" -> "3"
             "i" -> "1"
