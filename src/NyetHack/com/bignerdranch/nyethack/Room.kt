18ed0c1634c39936eb30d83c8cbafdfba0bb2cf4
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-21 01:55:39 +0900

    [2024-11-20] NyetHack 내용: 서브 클래스 생성

--- src/NyetHack/com/bignerdranch/nyethack/Room.kt
+++ src/NyetHack/com/bignerdranch/nyethack/Room.kt
@@ -1,7 +1,14 @@
 package NyetHack.com.bignerdranch.nyethack
 
-class Room(val name: String) {
-    fun description() = "Room: $name"
+open class Room(val name: String) {
+    protected open val dangerLevel = 5
 
-    fun load() = "아무도 여기에 오지 않았습니다..."
+    fun description() = "Room: $name\r\n" +
+            "위험 수준: $dangerLevel"
+
+    open fun load() = "아무도 여기에 오지 않았습니다..."
+}
+
+class TownSquare : Room("Town Square") {
+    override fun load() = "당신의 참여를 주민들이 다 함께 환영합니다!"
 }
