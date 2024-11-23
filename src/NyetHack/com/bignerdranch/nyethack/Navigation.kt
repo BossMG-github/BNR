861e26d5e27bd1d457bff8059fb90ee97b0f948f
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-23 14:28:58 +0900

    [2024-11-22] NyetHack 내용: enum 클래스

--- src/NyetHack/com/bignerdranch/nyethack/Navigation.kt
+++ src/NyetHack/com/bignerdranch/nyethack/Navigation.kt
@@ -1,5 +1,12 @@
 package NyetHack.com.bignerdranch.nyethack
 
+enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
+
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
 }
