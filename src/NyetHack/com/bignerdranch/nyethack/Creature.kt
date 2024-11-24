1334b9bb7c629e61a6d5b336c60afb9e1dfb0b24
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-25 00:22:17 +0900

    [2024-11-24] NyetHack 내용: NyetHack의 전투기능

--- src/NyetHack/com/bignerdranch/nyethack/Creature.kt
+++ src/NyetHack/com/bignerdranch/nyethack/Creature.kt
@@ -2,7 +2,7 @@ package NyetHack.com.bignerdranch.nyethack
 
 import java.util.*
 
-interface Fightable {
+interface Fightable { // 몬스터, 플레이어는 개념적으로 상속관계가 없음. 하지만 공통적인 속성이나 행동을 갖는 경우이기 때문에 인터페이스를 사용.
     var healthPoints: Int
     val diceCount: Int
     val diceSides: Int
@@ -14,7 +14,7 @@ interface Fightable {
     fun attack(opponent: Fightable): Int
 }
 
-abstract class Monster(
+abstract class Monster( // 상속 관계를 가지면서 인스턴ㅅ 생성이 필요 없는 경우면서 슈퍼 클래스가 필요한 상황이기 때문에 추상 클래스를 사용.
     val name: String,
     val description: String,
     override var healthPoints: Int) : Fightable {
