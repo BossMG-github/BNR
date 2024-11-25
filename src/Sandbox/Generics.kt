f7dc068b642c37b263c4d01dcbc30a537ac5bce6
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-25 17:54:24 +0900

    [2024-11-24] NyetHack 내용: 복합 제네릭 타입 매개변수

--- src/Sandbox/Generics.kt
+++ src/Sandbox/Generics.kt
@@ -1,7 +1,16 @@
 package Sandbox
 
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
+
+    fun <R> fetch(lootModFunction: (T) -> R) : R? {
+        return lootModFunction(loot).takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -11,4 +20,15 @@ class Coin(val value: Int)
 fun main(){
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("평범한 중절모", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("$name 를 LootBox에서 꺼냈습니다!")
+    }
+
+    val coin = lootBoxOne.fetch() {
+        Coin(it.value * 3)
+    }
+
+    coin?.let { println(it.value) }
 }
