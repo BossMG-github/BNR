1663919a1a0fd2e99663ad5bbfa2dde3082caafd
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-26 16:29:52 +0900

    [2024-11-25] Sandbox 내용: null 가능 타입의 확장 함수

--- src/Sandbox/Extensions.kt
+++ src/Sandbox/Extensions.kt
@@ -9,8 +9,14 @@ fun <T> T.easyPrint() : T {
     println(this)
     return this
 }
+
+infix fun String?.printWithDefault(default: String) = print(this ?: default)
+
 fun main() {
     "마드리갈이 그 건물에서 나왔습니다".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
     "How many vowels?".numVowels.easyPrint()
+
+    val nullableString: String? = null
+    nullableString printWithDefault "기본 문자열"
 }
