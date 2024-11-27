34291939c64cdbbc8624e1622e2229fdfdfd7205
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-28 00:31:17 +0900

    [2024-11-26] Interop 내용: 자바 클래스와 상호운용

---
+++ src/Interop/Hero.kt
@@ -0,0 +1,8 @@
+package Interop
+
+// NyetHack 왕국의 용사를 나타내는 파일
+
+fun main() {
+    val adversary = Jhava()
+    println(adversary.utterGreeting())
+}
---
+++ src/Interop/Jhava.java
@@ -0,0 +1,7 @@
+package Interop;
+// NyetHack이 아닌 다른 왕국의 괴물을 나타내는 클래스
+public class Jhava {
+    public String utterGreeting() {
+        return "BLARGH";
+    }
+}
