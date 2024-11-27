70abca85086fe01dd941965df326f4ea997a2ef0
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-11-27 20:09:54 +0900

    [2024-11-26] Sandbox 내용 : 함수형 프로그래밍 패러다임

--- src/Sandbox/FunctionalProgramming.kt
+++ src/Sandbox/FunctionalProgramming.kt
@@ -19,4 +19,18 @@ fun main() {
         (2 until number).map { number % it }.none { it == 0}
     }
     println(primes)
+//    ----------------------- 결합 --------------------------
+
+    val employees = listOf("Denny", "Claudette", "Peter")
+    val shirtSize = listOf("large", "x-large", "medium")
+    val employeeShirtSizes = employees.zip(shirtSize).toMap()
+    println(employeeShirtSizes["Denny"])
+
+    val foldedValue = listOf(1,2,3,4).fold(0) { accumulator, number ->
+        println("Accumulated value: $accumulator")
+        accumulator + (number * 3) // 누적값은 람다의 반환 즉 결과값으로 변경됨 , 이 부분의 결과로 바뀜
+        // 0 + (1 * 3)
+        // 3 + (2 * 3) 이런식으로.
+    }
+    println("Final value: $foldedValue")
 }
